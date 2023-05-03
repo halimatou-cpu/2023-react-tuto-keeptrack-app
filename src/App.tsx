98a2b94b6a7dbc699fd44cd05504ca3dd8023133
halimatou-cpu <halimatoubah.bh@gmail.com>
2023-05-03 02:18:17 +0200

    add & display mocked data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
   return (
-    <blockquote>
-      Tell me and I forget, teach me and I may remember, involve me and I learn. <br/>
-      {/* author */}
-      <h4>- Benjamin Franklin </h4>
-    </blockquote>
+    <div className='container'>
+      <ProjectsPage/>
+    </div>
   );
 }
 
