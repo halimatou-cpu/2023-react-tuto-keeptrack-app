7e84a24113b60bdac99e1e4ff72be698707b0f1f
halimatou-cpu <halimatoubah.bh@gmail.com>
2023-05-08 00:04:45 +0200

    Api faker & http calls

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,46 @@
-import React, { Fragment, useState } from "react";
-import { MOCK_PROJECTS } from "./MockProjects";
+import React, { Fragment, useEffect, useState } from "react";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
+import { projectAPI } from "./projectApi";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined | null>(undefined);
+
+  // const fetchProjects = () => {
+  //   return projectApi.get();
+  // };
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   projectApi
+  //   .get()
+  //   .then((data) => {
+  //     setError(null);
+  //     setLoading(false);
+  //     setProjects(data)
+  //   })
+  //   setLoading(false);
+  // }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    projectAPI
+      .get(1)
+      .then((data) => {
+        setError(null);
+        setLoading(false);
+        setProjects(data);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setError(e.message);
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      });
+  }, []);
 
   const saveProject = (project: Project) => {
     console.log("Saving project ", project);
@@ -17,7 +53,21 @@ function ProjectsPage() {
   return (
     <Fragment>
       <h1>Projects</h1>
+      {error && <div className="row">
+        <div className="card fluid error">
+          <section>
+            <p>
+              <span className="icon-alert inverse " />
+              {error}
+            </p>
+          </section>
+        </div>
+      </div>}
       <ProjectList projects={projects} onSave={saveProject} />
+      {loading && <div className="center-page">
+        <span className="spinner primary" />
+        <p>Loading...</p>
+      </div>}
     </Fragment>
   );
 }
