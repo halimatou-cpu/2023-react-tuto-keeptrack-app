afc5dd45675a9340471df3382bd2537cfcdbe509
halimatou-cpu <halimatoubah.bh@gmail.com>
2023-05-10 20:59:58 +0200

    project updated in db.json through API

--- src/projects/projectApi.ts
+++ src/projects/projectApi.ts
@@ -1,88 +1,91 @@
 import axios from "axios";
 import { Project } from "./Project";
+import { error } from "console";
 
 const baseUrl = "http://localhost:4000";
-const url = `${baseUrl}/projectvdvs`;
+const url = `${baseUrl}/projects`;
 
 function translateStatusToErrorMessage(status: number) {
 	switch (status) {
-	  case 401:
-		return 'Please login again.';
-	  case 403:
-		return 'You do not have permission to view the project(s).';
-	  default:
-		return 'There was an error retrieving the project(s). Please try again.';
+		case 401:
+			return 'Please login again.';
+		case 403:
+			return 'You do not have permission to view the project(s).';
+		case 404: 
+			return 'Project not found.'
+		default:
+			return 'There was an error retrieving the project(s). Please try again.';
 	}
-  }
+}
 
-  function checkStatus(response: any) {
+function checkStatus(response: any) {
 	if (response.ok) {
-	  return response;
+		return response;
 	} else {
-	  const httpErrorInfo = {
-		status: response.status,
-		statusText: response.statusText,
-		url: response.url,
-	  };
-	  console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
-  
-	  let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
-	  throw new Error(errorMessage);
+		const httpErrorInfo = {
+			status: response.status,
+			statusText: response.statusText,
+			url: response.url,
+		};
+		console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+		let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+		throw new Error(errorMessage);
 	}
-  }
+}
 
-  function parseJSON(response: Response) {
+function parseJSON(response: Response) {
 	return response.json();
-  }
-  
-  function delay(ms: number) {
+}
+
+function delay(ms: number) {
 	return function (x: any): Promise<any> {
-	  return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+		return new Promise((resolve) => setTimeout(() => resolve(x), ms));
 	};
-  }
+}
 
-  function convertToProjectModel(item: any): Project {
+function convertToProjectModel(item: any): Project {
 	return new Project(item);
-  }
-
+}
 
 function convertToProjectModels(data: any[]): Project[] {
 	let projects: Project[] = data.map(convertToProjectModel);
 	return projects;
-  }
+}
 
-  const projectAPI = {
+const projectAPI = {
 	get(page = 1, limit = 20) {
-	  return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-		.then(delay(600))
-		.then(checkStatus)
-		.then(parseJSON)
-		.then(convertToProjectModels)
-		.catch((error: TypeError) => {
-		  console.log('log client error ' + error);
-		  throw new Error(
-			'There was an error retrieving the projects. Please try again.'
-		  );
-		});
+		return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+			.then(delay(600))
+			.then(checkStatus)
+			.then(parseJSON)
+			.then(convertToProjectModels)
+			.catch((error: TypeError) => {
+				console.log('log client error ' + error);
+				throw new Error(
+					'There was an error retrieving the projects. Please try again.'
+				);
+			});
 	},
-  };
-  
-  export { projectAPI };
 
-// const projectApi = {
-// 	async get() {
-// 		try {
-// 			const response = await axios.get(url);
-// 			return response;
-// 		} catch (error: any) {
-// 			if (error.response) {
-// 				console.log(error.response.headers);
-// 				console.log(error.response.status);
-// 				console.log(error.response.data);
-// 			}
-// 			console.log(error.toJSON());
-// 		}
-// 	}
-// };
+	put(project: Project) {
+		return fetch(`${url}/${project.id}`, {
+			method: 'PUT',
+			body: JSON.stringify(project),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(checkStatus)
+			.then(parseJSON)
+			.catch((error: TypeError) => {
+				console.log('Log client error ' + error);
+				throw new Error(
+					'There was an error updating the project. Please try again.'
+				)
+			})
+	}
+};
 
-// export { projectApi };
+export { projectAPI };
+
